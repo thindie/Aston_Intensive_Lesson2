d2a3eb6a70ce0437cda2bcd3630dd4b759233621
Alex <thindie1@mail.ru>
2023-05-10 21:18:49 +0200

    initial commit

---
+++ app/src/main/java/com/example/thindie/aston_intensive_lesson2/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.thindie.aston_intensive_lesson2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
